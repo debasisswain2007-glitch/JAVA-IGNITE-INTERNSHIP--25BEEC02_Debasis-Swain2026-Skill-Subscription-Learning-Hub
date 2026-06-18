d4ec7a48d73bb7f989e823142d29cdaec30f5fd7
debasisswain2007-glitch <debasis.swain2007@gmail.com>
2026-06-18 21:48:32 +0530

    Update SkillPackServiceImpl.java  25BEEC02_Debasis Swain

--- Skill-Subscription-Learning-Hub-2026/src/main/java/com/skills/hub/service/impl/SkillPackServiceImpl.java
+++ Skill-Subscription-Learning-Hub-2026/src/main/java/com/skills/hub/service/impl/SkillPackServiceImpl.java
@@ -9,51 +9,54 @@ import java.util.List;
 
 @Service
 public class SkillPackServiceImpl implements SkillPackService {
-
+	//a private class instance is created to store all the packs and finalized
     private final SkillPackRepository packRepo;
 
+    //the pack is given and saved in the class instance
     public SkillPackServiceImpl(SkillPackRepository packRepo) {
         this.packRepo = packRepo;
     }
 
     @Override
     public SkillPack addSkillPack(SkillPack pack) {
-
-        // STEP 1: validate input
-        // STEP 2: save to DB
-        // STEP 3: return saved object
-
-        return null;
+    	//saves the pack given by the user
+        return packRepo.save(pack);
     }
 
     @Override
     public List<SkillPack> getAllPacks() {
-
-        // STEP 1: fetch all packs from DB
-        // STEP 2: return list
-
-        return null;
+    	//the packs stored, all of them are displayed
+        return packRepo.findAll();
     }
 
     @Override
     public SkillPack updateSkillPack(SkillPack pack) {
 
-        // STEP 1: find existing pack by ID
-        // STEP 2: if not found → return null
-        // STEP 3: update fields
-        // STEP 4: save updated pack
-        // STEP 5: return updated pack
+        System.out.println("Service Called");
+        System.out.println(pack.getId());
 
-        return null;
+        if (!packRepo.existsById(pack.getId())) {
+            System.out.println("Pack not found!");
+            return null;
+        }
+
+        return packRepo.save(pack);
     }
 
     @Override
     public void deleteSkillPack(Long id) {
+    	//it deletes the users pack by taking user id
+        packRepo.deleteById(id);
+    }
+    
+    @Override
+    public List<SkillPack> searchPacks(String keyword) {
 
-        // STEP 1: delete pack by ID
+        return packRepo.findByTitleContainingIgnoreCase(keyword);
+
     }
 
-	public SkillPackRepository getPackRepo() {
-		return packRepo;
-	}
+    public SkillPackRepository getPackRepo() {
+        return packRepo;
+    }
 }
